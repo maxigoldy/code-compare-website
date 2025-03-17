556b7721bd2512cc57f86813b9b7698ff76a30c7
Maxi Goldmann <info@maxigoldmann.de>
2025-03-17 13:36:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function compareTexts() {
+    const text1 = document.getElementById("text1").value.split("\n");
+    const text2 = document.getElementById("text2").value.split("\n");
+
+    let resultHTML1 = "";
+    let resultHTML2 = "";
+    let maxLength = Math.max(text1.length, text2.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        if (text1[i] === text2[i]) {
+            resultHTML1 += text1[i] + "\n";
+            resultHTML2 += text2[i] + "\n";
+        } else if (text1[i] === undefined) {
+            resultHTML2 += `<span class="added">+ ${text2[i]}</span>\n`;
+        } else if (text2[i] === undefined) {
+            resultHTML1 += `<span class="removed">- ${text1[i]}</span>\n`;
+        } else {
+            resultHTML1 += `<span class="removed">- ${text1[i]}</span>\n`;
+            resultHTML2 += `<span class="added">+ ${text2[i]}</span>\n`;
+        }
+    }
+
+    document.getElementById("result1").innerHTML = resultHTML1;
+    document.getElementById("result2").innerHTML = resultHTML2;
+}
